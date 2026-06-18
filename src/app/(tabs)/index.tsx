6b43225698d66e24599006abdb2b562f78daaa3f
Esteban Terrones <esteban20001118@gmail.com>
2026-06-18 18:01:36 -0500

    feat: implement meal tracking features

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
+import HomeHeader from '@/components/HomeHeader';
+import MacroGrid from '@/components/MacroGrid';
+import RecentMeals from '@/components/RecentMeals';
+import { getMeals, Meal } from '@/storage/meals';
 import { globalStyles } from '@/styles/global';
-import { Link } from 'expo-router';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
 import { ScrollView, Text } from 'react-native';
-import HomeHeader from '../../components/HomeHeader';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-     <HomeHeader />
-      <Link href='/add-meal' style={{ fontSize: 18, color: '#007bff' }}>
-        Go to Add Meal
-      </Link>
+      <HomeHeader />
+      <MacroGrid meals={meals} />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
